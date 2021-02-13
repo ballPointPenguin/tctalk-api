de7bff403d0594747327539bfdf7c218998c7e25
Benjamin Rosas <ben@aliencyb.org>
2021-02-12 22:08:28 -0600

    renew

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c8abad3447429ffc46ec49765835d9e'),
+    },
+  },
+});
